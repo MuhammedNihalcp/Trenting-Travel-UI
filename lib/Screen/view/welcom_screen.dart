83348271869dbfecf1c6651f2d709f6b6a15d54b
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-18 10:22:02 +0530

    frist init

---
+++ lib/Screen/view/welcom_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScreenWelcome extends StatelessWidget {
+  const ScreenWelcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/core/color/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const backgroundC = Colors.white;
