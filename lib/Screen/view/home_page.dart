fea98d46e42806eb7fdfbc4f7095eefdc2b0b15b
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-18 11:54:35 +0530

    add new images

--- lib/Screen/view/home_page.dart
+++ lib/Screen/view/home_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:tranting_travel_ui/core/color/colors.dart';
 
 class ScreenHome extends StatelessWidget {
   const ScreenHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: backgroundC,
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile()
+          ],
+        ),
+      ),
+    );
   }
 }
