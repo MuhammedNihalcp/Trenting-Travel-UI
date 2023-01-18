684681c1b37f4f45ca910435bea06a88a462f976
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-18 17:00:51 +0530

    add new navigator files

---
+++ lib/Screen/view/navigator.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/widgets.dart';
+
+class BottomNavigationBar extends StatelessWidget {
+  const BottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
