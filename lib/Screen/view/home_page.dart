1519a666ab1b474d13de9f1e6bd17265228309e1
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-18 13:13:49 +0530

    add new boy images on home page

--- lib/Screen/view/home_page.dart
+++ lib/Screen/view/home_page.dart
@@ -5,26 +5,65 @@ import 'package:tranting_travel_ui/core/color/colors.dart';
 import 'package:tranting_travel_ui/core/size/sizes.dart';
 
 class ScreenHome extends StatelessWidget {
-  const ScreenHome({Key? key, required this.width,required this.height,}):super(key: key);
-final double width;
-final double height;
+  const ScreenHome({
+    Key? key,
+    required this.width,
+    required this.height,
+  }) : super(key: key);
+  final double width;
+  final double height;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundC,
       body: SafeArea(
         child: Column(
-          children:  [
+          children: [
             kHeight10,
             const PersonalDetails(),
             CustomCard(
-              width: width * 0.8,
-              height: height * 0.3,
-              color: Colors.white,
+              width: width * 0.9,
+              height: height * 0.35,
+             
               elevation: 0,
-              child: Stack(children: [
-                Image(image: AssetImage('assets/image/Rectangle 10.png'),),
-              ],),
+              child: Stack(
+                children: [
+                  const Positioned(
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    child: Image(
+                      image: AssetImage('assets/image/Rectangle 10.png'),
+                    ),
+                  ),
+                  Positioned(
+                    top: height * 0.11,
+                    left: width * 0.13,
+                    child: const Image(
+                      image: AssetImage('assets/image/Frame.png'),
+                    ),
+                  ),
+                  Positioned(
+                    left: width * 0.28,
+                    child: const Image(
+                      image: AssetImage('assets/image/Frame-1.png'),
+                    ),
+                  ),
+                  Positioned(
+                    top: height * 0.16,
+                    left: width * 0.45,
+                    child: const Image(
+                      image: AssetImage('assets/image/Frame-2.png'),
+                    ),
+                  ),
+                  Positioned(
+                    left: width * 0.1,
+                    child:const Image(
+                      image: AssetImage('assets/image/Frame-3.png'),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
