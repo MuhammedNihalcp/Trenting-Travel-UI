6155093ee14146a790b2bb5e3236d0122c885abb
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-18 11:23:30 +0530

    add image on welcome screen

--- lib/Screen/view/welcom_screen.dart
+++ lib/Screen/view/welcom_screen.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tranting_travel_ui/core/color/colors.dart';
 
 class ScreenWelcome extends StatelessWidget {
-  const ScreenWelcome({super.key});
-
+  ScreenWelcome({super.key});
+  final height = Get.size.height;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundC,
-     
+      body: SafeArea(
+          child: Container(
+        width: double.infinity,
+        height: height,
+        decoration:const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/image/welcom_page_image.png'),
+              fit: BoxFit.cover),
+        ),
+      )),
     );
   }
 }
