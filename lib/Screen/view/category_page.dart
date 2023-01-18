c86b1b630f5f8bad49ff69d268ae6f2fd137be80
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-18 19:39:23 +0530

    update category grid view

--- lib/Screen/view/category_page.dart
+++ lib/Screen/view/category_page.dart
@@ -18,75 +18,94 @@ class ScreenCategory extends StatelessWidget {
     'assets/image/category3.png',
     'assets/image/category4.png',
   ];
+  List<String> placeDetails = [
+    'Udaipur, also known as the City of Lakes, is the crown jewel of the state of Rajasthan. It is surrounded by the beautiful Aravalli Hills',
+    'The moment you set foot in Jaipur, you will feel like everything has come straight out of the pages of some epic novel.',
+    'Lagoons of crystal clear water and scenic beaches topped with a little bit of history make Andaman and Nicobar one of the most perfect choices for an enjoyable',
+    'A city as old as time itself. A city that has stayed in place and watched as the world has turned and the tides have changed. A city that has stood the test of time for thousands of years',
+    'Widely considered to be the best place to visit in India among youths, Goa is the epitome of tourism in India with one of the best beaches, food and nightlife',
+    'Home to one of the 7 wonders of the world, the Taj Mahal, Agra is a sneak peek into the architectural history with other structures',
+    'The idyllic hill station Munnar - famous for its tea estates, exotic lush greenery and craggy peaks,',
+    'aw-dropping locales, mesmerising sunrises, the untouched beauty of the hills, the old-world charm of the past, and the welcoming smiles of the local people',
+  ];
+  List<String> placeName = [
+    ' City Palace, Udaipur',
+    ' Amer Fort',
+    'Cellular Jail',
+    ' Kashi Vishwanath Temple',
+    ' Calangute Beach',
+    ' Shopping in Agra',
+    ' Munnar Tea Plantations ',
+    ' Om Beach',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundC,
       body: SafeArea(
-          child: (GridView.builder(
-        padding: EdgeInsets.zero,
-        physics: const NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 5,
-            mainAxisSpacing: 5,
-            childAspectRatio: 1 / 1.7),
-        itemBuilder: (BuildContext ctx, int index) {
-          return SizedBox(
-            // width: width * 0.1,
-            // color: Colors.blue,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Stack(
-                    children: [
-                      InkWell(
-                        onTap: () {},
-                        child: Container(
-                          width: width * 0.5,
-                          height: height * 0.28,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: colorWhite,
-                            image: DecorationImage(
-                              image: AssetImage(gridImage[index]),
-                              // fit: BoxFit.cover,
+        child: GridView.builder(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 5,
+              childAspectRatio: 1 / 1.5),
+          itemBuilder: (BuildContext ctx, int index) {
+            return SizedBox(
+              // width: width * 0.1,
+              // color: Colors.blue,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Stack(
+                      children: [
+                        InkWell(
+                          onTap: () {},
+                          child: Container(
+                            width: width * 0.5,
+                            height: height * 0.28,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: colorWhite,
+                              image: DecorationImage(
+                                image: AssetImage(gridImage[index]),
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
                         ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      placeName[index],
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    '',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(fontWeight: FontWeight.w400),
-                  ),
-                  kHeight10,
-                  Row(
-                    children: [
-                      Text(
-                        "",
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
+                    ),
+                    kHeight10,
+                    Text(
+                      placeDetails[index],
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          );
-        },
-        itemCount: gridImage.length,
-      ))),
+            );
+          },
+          itemCount: gridImage.length,
+        ),
+      ),
     );
   }
 }
