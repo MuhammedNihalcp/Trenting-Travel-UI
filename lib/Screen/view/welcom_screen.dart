c5781d98a72b8660cd827d7f70eedb08641ad1ad
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-18 11:45:35 +0530

    add buttom on welcome screen

--- lib/Screen/view/welcom_screen.dart
+++ lib/Screen/view/welcom_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tranting_travel_ui/core/color/colors.dart';
+import 'package:tranting_travel_ui/core/size/sizes.dart';
 
 class ScreenWelcome extends StatelessWidget {
   ScreenWelcome({super.key});
@@ -23,14 +24,32 @@ class ScreenWelcome extends StatelessWidget {
               ),
             ),
             Positioned(
-                child: ElevatedButton(
-                    onPressed: () {},
-                    child: Row(
-                      children: [
-                        Text("Let's Start"),
-                        Icon(Icons.arrow_forward),
-                      ],
-                    )))
+              bottom: height * 0.1,
+              left: width * 0.2,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: buttonC,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    minimumSize: Size(width * 0.6, height * 0.1)),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Let's Start",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    kWidth10,
+                    Icon(Icons.arrow_forward_rounded),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
