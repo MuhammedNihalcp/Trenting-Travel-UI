e6c10c684d496a255a7ed175681b50def88a949b
MuhammedNihalcp <nihalriju915@gmail.com>
2023-01-18 19:49:03 +0530

    place view updated

--- lib/Screen/view/place_view.dart
+++ lib/Screen/view/place_view.dart
@@ -1,12 +1,32 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:tranting_travel_ui/core/color/colors.dart';
 
 class ScreenPlaceView extends StatelessWidget {
-  const ScreenPlaceView({super.key});
+  const ScreenPlaceView({Key? key, required this.width, required this.height})
+      : super(key: key);
+
+  final double width;
+  final double height;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: backgroundC,
+      body: SafeArea(
+          child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: height * 0.4,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/image/ladaku.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
